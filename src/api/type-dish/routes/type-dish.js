f81285dd7e7ec75d8cef3160c60b7e4c28e75594
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-17 17:47:24 +0200

    🔧 Remove type-dish schema and controller
    This commit removes the type-dish schema, controller, and related documentation files. These changes are no longer needed in the codebase.

--- src/api/type-dish/routes/type-dish.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * type-dish router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::type-dish.type-dish');
--- src/api/type-dish/services/type-dish.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * type-dish service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::type-dish.type-dish');
