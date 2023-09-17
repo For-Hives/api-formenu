59e9215af35f9fab50597e334d03f90bfa1d3b1c
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-17 17:22:24 +0200

    🔥 Remove type-dish.json
    This commit removes the type-dish.json file, which was no longer needed in the codebase.

--- src/api/type-dish/controllers/type-dish.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * type-dish controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::type-dish.type-dish');
