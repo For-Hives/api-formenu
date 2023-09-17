0434f2dd94f59b2a7360da945f8204b40c8825bf
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-17 17:48:25 +0200

    🔧 Add type-dish schema and controller
    - Added a new schema for type-dish in the dish content-type
    - Created a new controller for type-dish
    - Updated the documentation for type-dish API endpoints

---
+++ src/api/type-dish/controllers/type-dish.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * type-dish controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::type-dish.type-dish');
---
+++ src/api/type-dish/services/type-dish.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * type-dish service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::type-dish.type-dish');
