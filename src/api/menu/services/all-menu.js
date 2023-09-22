953c3d49770c3c065a0867face30aa932e984693
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-22 13:39:11 +0200

    🔧 Update all-menu.js and full_documentation.json
    - In all-menu.js, added fields to be fetched from the menu entity and its related entities.
    - In full_documentation.json, updated the x-generation-date field.

--- src/api/menu/services/all-menu.js
+++ src/api/menu/services/all-menu.js
@@ -9,23 +9,99 @@ module.exports = {
     const data = await strapi.entityService.findMany(
       "api::menu.menu",
       {
+        fields: [
+          'id',
+          'title',
+          'description',
+          'activated',
+          'locale'
+        ],
         populate: {
-          company: {
+          image: {
             populate: '*'
           },
-          categories: {
+          company: {
+            fields: [
+              'id',
+              'name',
+              'description',
+              'country',
+              'city',
+              'street',
+              'postcode'
+            ],
             populate: {
-              category:{
+              logo: {
                 populate: '*'
               },
-              categories:{
+              background: {
                 populate: '*'
+              },
+              // menus:{
+              //   populate: '*'
+              // }
+            }
+          },
+          categories: {
+            fields: [
+              'id',
+              'name',
+              'locale',
+              'depth',
+              'order'
+            ],
+            populate: {
+              icon: {
+                populate: '*'
+              },
+              category: {
+                fields: [
+                  'id',
+                  'name',
+                  'locale',
+                  'depth',
+                  'order'
+                ],
+                populate: {
+                  icon: {
+                    populate: '*'
+                  }
+                }
+              },
+              categories: {
+                fields: [
+                  'id',
+                  'name',
+                  'locale',
+                  'depth',
+                  'order'
+                ],
+                populate: {
+                  icon: {
+                    populate: '*'
+                  }
+                }
               },
               dishes: {
                 populate: {
-                  dishes:{
+                  dishes: {
                     populate: '*'
                   },
+                  image: {
+                    populate: '*'
+                  },
+                  type_dish: {
+                    fields:[
+                      'id',
+                      'name',
+                      'color',
+                    ],
+                    populate: {
+                      icon: {
+                        populate: '*'
+                      }
+                    }
+                  }
                 }
               }
             }
