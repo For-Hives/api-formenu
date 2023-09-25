54128f607ee67dc0de99348909e3c4c384f31189
breval <breval2000@live.fr>
2023-09-26 01:58:41 +0200

    Add API URL to server configuration
    This commit adds the API URL 'https://formenu.fr/api' to the server configuration file.

--- config/server.js
+++ config/server.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: 'https://formenu.fr/api',
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
