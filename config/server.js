15a76a03eb31c75d3c12fb545cd465155d867fba
Cinquin Andy <andy.cinquin@infotel.com>
2023-10-01 14:30:51 +0200

    🔧 Update API URL and generation date
    - Updated the `.env.example` file to replace `ADMIN_URL` with `API_URL`.
    - Modified the `config/server.js` file to use the new `API_URL` environment variable.
    - Changed the value of `"x-generation-date"` in `full_documentation.json`.
    
    This commit updates the API URL and generation date for improved functionality.

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env("ADMIN_URL"),
+  url: env("API_URL"),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
