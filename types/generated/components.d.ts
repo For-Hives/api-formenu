e1b000981362fac1270ea3bbcb2d0ca0f24713bc
CinquinAndy <cinquin.andy@gmail.com>
2023-10-23 15:20:55 +0200

    🔧 Update full_documentation.json and components.d.ts
    - Updated the "x-generation-date" field in full_documentation.json to "2023-10-23T13:20:39.981Z"
    - Added a new interface "ContentWebsiteContentWebsite" in components.d.ts
    - Added attributes to the "ContentWebsiteContentWebsite" interface

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,25 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ContentWebsiteContentWebsite extends Schema.Component {
+  collectionName: 'components_content_website_content_websites';
+  info: {
+    displayName: 'Content_website';
+    icon: 'archive';
+  };
+  attributes: {
+    metadata_title: Attribute.String;
+    metadata_description: Attribute.String;
+    home_title: Attribute.String;
+    home_subtitle: Attribute.String;
+    home_background_images: Attribute.Media;
+    home_image: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'content-website.content-website': ContentWebsiteContentWebsite;
+    }
+  }
 }
