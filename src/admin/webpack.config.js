f28a2339d4da35c6abc18551f2640793ca9bd2db
Cinquin Andy <andy.cinquin@infotel.com>
2023-12-10 19:45:16 +0100

    delete useless plugin & fix webpack

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-
 module.exports = (config) => {
-  config.plugins.push(new MonacoWebpackPlugin());
-
   return config;
 };
