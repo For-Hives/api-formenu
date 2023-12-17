912a7dd4222c583369b4cd38523848e805c2f89e
Cinquin Andy <andy.cinquin@infotel.com>
2023-12-17 18:13:05 +0100

    🔧 Update custom-ingredient.js and full_documentation.json
    - Refactor findMyIngredients function in custom-ingredient.js to handle user population
    - Add error handling for undefined user or user's company
    - Update x-generation-date in full_documentation.json to "2023-12-17T17:12:47.206Z"

--- src/api/ingredient/services/custom-ingredient.js
+++ src/api/ingredient/services/custom-ingredient.js
@@ -3,8 +3,14 @@
 module.exports = {
     findMyIngredients: async (user) => {
         // Récupérer l'ID de la société associée à l'utilisateur
-        console.log("(services) findMyIngredients - user", user)
-        const companyId = user.company.id;
+        const userPopulated = await strapi.entityService.findOne('plugin::users-permissions.user', user.id, {
+            populate: { company: true },
+        });
+        console.log("(services) findMyIngredients - userPopulated", userPopulated)
+        if (!userPopulated || !userPopulated.company) {
+            throw new Error("User or user's company is undefined");
+        }
+        const companyId = userPopulated.company.id;
 
         // Utiliser Strapi pour requêter la base de données
         const ingredients = await strapi.entityService.findMany(
