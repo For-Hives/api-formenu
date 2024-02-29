0e28587e2576ba71b1c8c2ae454cafb13c09b825
Cinquin Andy <andy.cinquin@infotel.com>
2024-02-29 19:44:26 +0100

    ✨ Add functionality for finding user's ingredients
    Added new files and functions to handle fetching a user's ingredients based on their company association.Implemented routes, controllers, and services to support this feature.

---
+++ src/api/dish/controllers/custom-dish.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+    async findMyIngredients(ctx, next) {
+        try {
+            const user = ctx.state.user; // ou ctx.request.user, selon votre configuration d'authentification
+
+            if (!user) {
+                return ctx.badRequest("No authenticated user found");
+            }
+
+            const data = await strapi
+                .service("api::ingredient.custom-ingredient")
+                .findMyIngredients(user);
+
+            ctx.body = data;
+        } catch (err) {
+            ctx.badRequest("Error in fetching ingredients by company", {
+                moreDetails: err.message,
+            });
+        }
+    },
+};
---
+++ src/api/dish/routes/custom-dish.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = {
+    routes: [
+        {
+            method: "GET",
+            path: "/my-ingredients",
+            handler: "custom-ingredient.findMyIngredients",
+            config: {
+                policies: [],
+                middlewares: [
+                    "api::ingredient.company-tenancy"
+                ],
+            },
+        },
+    ],
+};
