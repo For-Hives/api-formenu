0699811bf180c292db5d17e2ccb7c45d8d57f151
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-03 18:13:06 +0100

    ✨ Refactor category controller method names
    Changed method name and service call to handle categories instead of ingredients for fetching data. Improved error message clarity.

--- src/api/category/controllers/custom-category.js
+++ src/api/category/controllers/custom-category.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-    async findMyIngredients(ctx, next) {
+    async findMyCategories(ctx, next) {
         try {
             const user = ctx.state.user; // ou ctx.request.user, selon votre configuration d'authentification
 
@@ -10,12 +10,12 @@ module.exports = {
             }
 
             const data = await strapi
-                .service("api::ingredient.custom-ingredient")
-                .findMyIngredients(user);
+                .service("api::category.custom-category")
+                .findMyCategories(user);
 
             ctx.body = data;
         } catch (err) {
-            ctx.badRequest("Error in fetching ingredients by company", {
+            ctx.badRequest("Error in fetching categories by company", {
                 moreDetails: err.message,
             });
         }
